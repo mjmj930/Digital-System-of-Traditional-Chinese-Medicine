38d8fa20b5243f9e3b3b66a3665a5a12d898004e
mjmj <gaifu224@outlook.com>
2024-04-07 17:37:49 +0800

    Automated commit message

---
+++ git_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 进入你的项目目录，替换为你的项目路径
+cd D:/Digital-System-of-Traditional-Chinese-Medicine
+
+# 添加所有变化到Git
+git add .
+
+# 提交变更，可以修改commit信息
+git commit -m "Automated commit message"
+
+# 推送到远程仓库的main分支，你可以根据需要修改分支名
+git push origin main
